3d7cfc77ebe5141769e27715a38b15768e2b6ee6
Abdelrahman Abdelnabi <abdoumohamed96@gmail.com>
2019-06-01 05:42:35 +0200

    first version of a full working cycle

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, jsonify,request
+from preprocessing.utils import get_video_frames
+from preprocessing.transformer import ResNextTransformer
+from preprocessing.spatial_transforms import validation_spatial_transform
+import torch
+from torchvision import transforms
+import numpy as np
+from model.model_zoo import ResnextClassifier
+import os
+import matplotlib.image as mpimg
 
 app = Flask(__name__)
+
+model = ResnextClassifier('best_params_100_deep_64.pth')
+
+@app.after_request
+def allow_CORS(response):
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  return response
 
 @app.route("/")
 def home():
@@ -8,10 +24,28 @@ def home():
 
 @app.route("/predict", methods=['POST'])
 def predict():
-  print('request to predict')
   video = request.files['video']
-  video.save('video.mp4')
-  return jsonify({ "prediction": "1" })
+  file_path = 'video.avi'
+  video.save(file_path)
+  frames = get_video_frames(file_path)
+  if os.path.isfile(file_path):
+    os.remove(file_path)
+
+  transformer = ResNextTransformer(validation_spatial_transform, 64, 112)
+  transformed_frames = transformer(frames)
+  transformed_frames = torch.unsqueeze(transformed_frames, 0)
+  predictions = np.array(model.predict(transformed_frames).squeeze())
+
+  k = 5
+  top_k = np.array(predictions).argsort()[-k:][::-1]
+  res = {
+    "predictions": [{
+      "id": str(id),
+      "probability": str(prob)
+      } for id, prob in zip(top_k, predictions[top_k])]
+    }
+  print(res)
+  return jsonify(res)
 
 if __name__ == "main":
-  app.run(debug=True)
+  app.run()
